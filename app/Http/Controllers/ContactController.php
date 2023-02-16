c5c43c549a36f3f11d5c63e8f096405ba6874fdc
Lelkes István <47304380+ilelkes@users.noreply.github.com>
2023-02-16 18:18:13 +0100

    Sortable columns
    p 109

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -17,11 +17,13 @@ class ContactController extends Controller
     public function index()
     {
         $companies = $this->company->pluck();
+
         $contacts = Contact::allowedTrash()
-            ->allowedSorts('first_name')
+            ->allowedSorts(['first_name', 'last_name', 'email'], "-id")
             ->allowedFilters('company_id')
             ->allowedSearch('first_name', 'last_name', 'email')
             ->paginate(10);
+
         return view('contacts.index', compact('contacts', 'companies'));
     }
 
