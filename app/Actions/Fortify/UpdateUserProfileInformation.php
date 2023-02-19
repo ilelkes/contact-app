2d92fba193e27d37abb2a0b095acb5d021dc062b
Lelkes István <47304380+ilelkes@users.noreply.github.com>
2023-02-19 14:58:39 +0100

    File Storage
    Settings - Edit Profile

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -4,6 +4,7 @@ namespace App\Actions\Fortify;
 
 use App\Models\User;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
@@ -34,6 +35,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             'profile_picture' => ['nullable', 'image'],
         ])->validate();
 
+        $this->uploadProfilePicture($input);
+
         if (
             $input['email'] !== $user->email &&
             $user instanceof MustVerifyEmail
@@ -47,7 +50,27 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
                 'company' => $input['company'],
                 'country' => $input['country'],
                 'address' => $input['address'],
+                'profile_picture' => $input['profile_picture'],
             ])->save();
+        }
+    }
+
+    protected function uploadProfilePicture(&$input)
+    {
+        if (request()->hasFile('profile_picture')) {
+            $uploadedFile = $input['profile_picture'];
+            
+            // dump($uploadedFile->getClientOriginalName());
+            // dump($uploadedFile->getClientOriginalExtension());
+            // dd($uploadedFile->getClientMimeType());
+
+            $fileName = $uploadedFile->storeAs(
+                'profile',
+                'profile-user-' . request()->user()->id .
+                    '.' .
+                    $uploadedFile->getClientOriginalExtension()
+            );
+            $input['profile_picture'] = $fileName;
         }
     }
 
@@ -65,6 +88,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             'company' => $input['company'],
             'country' => $input['country'],
             'address' => $input['address'],
+            'profile_picture' => $input['profile_picture'],
             'email_verified_at' => null,
         ])->save();
 
--- resources/views/settings/profile-information.blade.php
+++ resources/views/settings/profile-information.blade.php
@@ -9,7 +9,7 @@
                 @include('settings._sidenav')
 
                 <div class="col-md-9">
-                    <form action="{{ route('user-profile-information.update') }}" method="POST">
+                    <form action="{{ route('user-profile-information.update') }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         @method('PUT')
                         <div class="card">
@@ -66,7 +66,7 @@
                                             <div class="fileinput fileinput-new" data-provides="fileinput">
                                                 <div class="fileinput-new img-thumbnail"
                                                     style="width: 150px; height: 150px;">
-                                                    <img src="https://via.placeholder.com/150x150" alt="...">
+                                                    <img src="{{ $user->profilePictureUrl() }}" alt="...">
                                                 </div>
                                                 <div class="fileinput-preview fileinput-exists img-thumbnail"
                                                     style="max-width: 150px; max-height: 150px;"></div>
@@ -74,7 +74,7 @@
                                                     <span class="btn btn-outline-secondary btn-file"><span
                                                             class="fileinput-new">Select
                                                             image</span><span class="fileinput-exists">Change</span><input
-                                                            type="file" name="profile_picture"></span>
+                                                            type="file" name="profile_picture" accept="image/*"></span>
                                                     <a href="#" class="btn btn-outline-secondary fileinput-exists"
                                                         data-dismiss="fileinput">Remove</a>
                                                 </div>
